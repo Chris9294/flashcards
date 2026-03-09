5e8d908e72c01ceb6faedd4902d71f7efa07ce3f
Chris9294 <christophepaxion@sfr.fr>
2026-03-09 22:47:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -259,22 +259,34 @@ function startMemory(){
         firstCard={div,card};
       }else{
         secondCard={div,card};
-        if(firstCard.card.pairId===secondCard.card.pairId){
-          showCheck();
-          matchedPairs++;
-          firstCard=null;
-          secondCard=null;
-          if(matchedPairs===totalPairs){
-            showBravo();
-          }
-        }else{
-          setTimeout(()=>{
-            hideCard(firstCard.div);
-            hideCard(secondCard.div);
-            firstCard=null;
-            secondCard=null;
-          },1000);
-        }
+      if(firstCard.card.pairId===secondCard.card.pairId){
+
+  showCheck();
+  matchedPairs++;
+
+  // faire disparaître les cartes au lieu de les laisser visibles
+  setTimeout(()=>{
+    firstCard.div.remove();
+    secondCard.div.remove();
+
+    firstCard=null;
+    secondCard=null;
+
+    // si toutes les paires trouvées, bravo
+    if(matchedPairs===totalPairs){
+      showBravo();
+    }
+  },400); // léger délai pour voir le check
+
+} else {
+  setTimeout(()=>{
+    hideCard(firstCard.div);
+    hideCard(secondCard.div);
+
+    firstCard=null;
+    secondCard=null;
+  },1000);
+}
       }
     };
     cardContent.appendChild(div);
